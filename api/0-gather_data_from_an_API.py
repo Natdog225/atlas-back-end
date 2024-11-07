e0855e1550765a53f9751f13e5cf0842bbcfba2c
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-07 12:33:29 -0600

    Added a slash

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -19,7 +19,7 @@ def display_progress(employee_data):
                        for task in employee_data if task['completed']]
     total_tasks = len(employee_data)
     employee_name = employee_data[0].get('username')
-    print(f"Employee {employee_name} is done with tasks
+    print(f"Employee {employee_name} is done with tasks\
           ({len(completed_tasks)}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task}")
