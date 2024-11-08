b9488a63dde595c8d2e70b287382a480a69430bb
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-08 10:37:47 -0600

    .

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,12 +16,12 @@ def fetch_employee_data(employee_id):
 
 def display_progress(employee_data):
     """Displays the employee's TODO list progress."""
-    completed_tasks = [task['title'] for\
+    completed_tasks = [task['title'] for
         task in employee_data if task['completed']]
     total_tasks = len(employee_data)
     employee_name = employee_data[0].get('username')
     print(f"Employee {employee_name} is done with tasks"
-          f"({len(completed_tasks)}/{total_tasks}):")
+            f"({len(completed_tasks)}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task}")
 
